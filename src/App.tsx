e02049535defc18f17eed848d98a6b21718fbaa8
Guilherme Campos <guilhermeZA.123123@gmail.com>
2021-12-13 15:39:57 -0300

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { ContainerContent, GlobalStyle } from './GlobalStyle';
+import { io } from 'socket.io-client';
+import Peer from 'peerjs';
+
+const SocketConnection = io('http://localhost:3234/peerConnection', { transports: ['websocket'] })
 
 function App() {
+
+  const peer = new Peer();
+
+  const Success = (stream: MediaStream) => {
+    const Container = document.getElementById('Container');
+    const videoElement = document.createElement('video');
+    videoElement.srcObject = stream;
+    videoElement.autoplay = true;
+
+    Container!.appendChild(videoElement);
+
+    SocketConnection.on('get_main_peer', (id: string) => {
+      console.log(id, stream);
+      peer.call(id, stream);
+    })
+  }
+
+  const ErrorCath = (error: any) => {
+    console.log(error);
+  }
+
+  useEffect(() => {
+    navigator.mediaDevices.getDisplayMedia({
+      video: true
+    }).then(Success, ErrorCath);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <ContainerContent id="Container">
+
+      </ContainerContent>
+    </>
   );
 }
 
