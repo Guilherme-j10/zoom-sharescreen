0d7855f0ead007808611e722d4e02cc1c7f69b1d
Guilherme Campos <guilhermeZA.123123@gmail.com>
2021-12-14 17:09:01 -0300

    add some commits

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,23 @@ import { ContainerContent, GlobalStyle } from './GlobalStyle';
 import { io } from 'socket.io-client';
 import Peer from 'peerjs';
 
-const SocketConnection = io('https://zoom-shareserver.herokuapp.com/peerConnection', { transports: ['websocket'] })
+const SocketConnection = io('https://zoomsocket.cloudcall.tec.br/peerConnection', { transports: ['websocket'] })
 
 function App() {
 
-  const peer = new Peer();
+  const peer = new Peer({
+    host: 'zoombk.cloudcall.tec.br',
+    port: 3234,
+    secure: true,
+    path: '/peerAplication',
+    debug: 3,
+    config: {
+      iceServers: [
+        { urls: 'turn:34.69.41.255:3478?transport=tcp', credential: 'false', username: 'megaconecta:m364c0n3c74' },
+        { urls: 'turn:34.69.41.255:3478?transport=udp', credential: 'false', username: 'megaconecta:m364c0n3c74' }
+      ]
+    }
+  });
 
   const Success = (stream: MediaStream) => {
     const Container = document.getElementById('Container');
@@ -17,10 +29,14 @@ function App() {
 
     Container!.appendChild(videoElement);
 
-    SocketConnection.on('get_main_peer', (id: string) => {
-      console.log(id, stream);
+    SocketConnection.on('get_main_peer_from_reciver', (id) => {
       peer.call(id, stream);
-    })
+    });
+
+    SocketConnection.emit('peer_main');
+    SocketConnection.on('get_main_peer', (id: string) => {
+      peer.call(id, stream);
+    });
   }
 
   const ErrorCath = (error: any) => {
